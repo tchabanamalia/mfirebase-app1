8972aafacac291b4d34e84f16695c75ce5c25a39
tchabanamalia <tchabanamalia@gmail.com>
2022-12-04 10:35:28 +0000

    m firebase 1

---
+++ storage/framework/views/3be9410a749f97aa3dd46daf0ff70a0dffba07cd.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>m-firebase-app</title>
+</head>
+<body>
+    <h2 style="text-align: center">Hello firebase app!</h2>
+</body>
+</html>
+<?php /**PATH /home/malia29/projet_git/mfirebase-app1/resources/views/welcome.blade.php ENDPATH**/ ?>
